f188d74f1a78c086ba8a89fab9c46ee8ec1211fd
Matthew Gould <matt@everettgould.com>
2016-10-26 22:41:52 -0700

    adding up base 5 to base 10

--- ex09/add_chelou.sh
+++ ex09/add_chelou.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
-n1=( ["'"]=5 ['\\']=6 ['"']=7 ['?']=8 ['!']=9 )
-n2=( ['m']=0 ['r']=1 ['d']=2 ['o']=3 ['c']=4 )
+output1="$(echo $FT_NBR1 | sed -e "s/\'/0/g" -e 's/\\/1/g' -e 's/\"/2/g' -e 's/\?/3/g' -e "s/\!/4/g")"
 
-for ((i = 0; i<${#FT_NBR1}; i++)); 
-do 
-    echo ${n1[${FT_NBR1:$i:1}]} 
-done
+output2="$(echo $FT_NBR2 | sed -e 's/m/0/g' -e 's/r/1/g' -e 's/d/2/g' -e 's/o/3/g' -e "s/c/4/g")"
 
-for ((i = 0; i<${#FT_NBR2}; i++)); 
-do 
-    echo ${n2[${FT_NBR2:$i:1}]} 
-done
+#echo "${output1}"
+#echo "${output2}"
+
+o1="$(echo "ibase=5; ${output1}" | bc)"
+o2="$(echo "ibase=5; ${output2}" | bc)"
+
+#echo "${o1}"
+#echo "${o2}"
+
+sum="$(echo "${o1} + ${o2}" | bc)"
+#echo "${sum}"
+
+con="$(echo "obase=13;${sum}" | bc)"
+#echo "${con}"
+
+echo ${con} | sed -e 's/0/g/g' -e 's/1/t/g' -e 's/2/a/g' -e 's/3/i/g' -e 's/4/o/g' -e 's/5/ /g' -e 's/6/l/g' -e 's/7/u/g' -e 's/8/S/g' -e 's/9/n/g' -e 's/A/e/g' -e 's/B/m/g' -e 's/C/f/g' 
