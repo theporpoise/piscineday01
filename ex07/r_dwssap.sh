193c52943d8c0a146186a1c5b2ba56bc84842f5b
Matthew Gould <matt@everettgould.com>
2016-10-26 22:23:28 -0700

    updated the function in 7 so that there is a comma and a space between each item of output.  previously forgot the space.

--- ex07/r_dwssap.sh
+++ ex07/r_dwssap.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cat /etc/passwd | awk '!/^#/ {print}' | awk 'NR % 2 == 0 {print}' | cut -d ':' -f 1 | rev | sort -r | awk -v ft1="$FT_LINE1" -v ft2="$FT_LINE2" '{if ((ft2 >= ft1) && (NR>=ft1 && NR<=ft2)) print; else if ((ft1 >= ft2) && (NR>=ft2 && NR<=ft1)) print}' | tr '\n' ',' | sed 's/,$/\./'
+cat /etc/passwd | awk '!/^#/ {print}' | awk 'NR % 2 == 0 {print}' | cut -d ':' -f 1 | rev | sort -r | awk -v ft1="$FT_LINE1" -v ft2="$FT_LINE2" '{if ((ft2 >= ft1) && (NR>=ft1 && NR<=ft2)) print; else if ((ft1 >= ft2) && (NR>=ft2 && NR<=ft1)) print}' | tr '\n' ',' | sed 's/,/, /g' | sed 's/, $/\./'
